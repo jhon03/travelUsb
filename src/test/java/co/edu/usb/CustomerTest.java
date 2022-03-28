278c48925e7651de2fd10e06e1a5a8db34180ca1
jhon03 <hoyosb90@gmail.com>
2022-03-27 20:37:19 -0500

    primer commit

--- src/test/java/co/edu/usb/CustomerTest.java
+++ src/test/java/co/edu/usb/CustomerTest.java
@@ -151,9 +151,29 @@ void searchCustomerForRangeDate() throws SQLException{
 			} catch (Exception e) {
 				// TODO: handle exception
 			}
+		}
+@Test			
+void imprimirNumeros() {
+	
+     
+      
+	  for(int i = 0; i<=13; i++) {
 		
-	}
-	}
+		  if(i % 2 != 0) {
+			  System.out.println(i);
+		  }else {
+			  System.out.println(i + "not  is prime");
+		  }
+		  
+
+	  }
+	
+
+		}
+		
+}
+	
+	
 
 	
 

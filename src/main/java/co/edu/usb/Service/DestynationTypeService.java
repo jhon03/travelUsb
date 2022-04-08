57e9c297f732e8b6254ef514bf8dd292cbd0e015
jhon03 <hoyosb90@gmail.com>
2022-04-07 23:28:43 -0500

    update abril 7

--- src/main/java/co/edu/usb/Service/DestynationTypeService.java
+++ src/main/java/co/edu/usb/Service/DestynationTypeService.java
@@ -2,9 +2,6 @@ package co.edu.usb.Service;
 
 import co.edu.usb.domain.DestinationType;
 
-import java.sql.SQLException;
-import java.util.List;
-
 public interface DestynationTypeService {
 
     public DestinationType findByCodeAndStatus (String Code, String Status) throws Exception;
